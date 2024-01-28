5750a5268ba0ae78c6a1b89f3c9b3afa2fd4efa9
woongminKi <dndals1991@gmail.com>
2024-01-28 16:51:37 +0900

    feature: socket 및 기본 라우터

--- app.js
+++ app.js
@@ -1,39 +1,42 @@
+require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const cors = require('cors');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-
+const index = require('./routes/index');
+const users = require('./routes/users');
+const join = require('./routes/join');
 const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+const corsOptions = {
+  origin: process.env.COZY_CLIENT_URL,
+  credentials: true,
+};
+app.use(cors(corsOptions));
+app.use(express.json({ limit: 5000000 }));
+app.use(express.urlencoded({ limit: 5000000, extended: false }));
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/index', index);
+app.use('/', join);
+app.use('/users', users);
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
--- routes/join.js
+++ routes/join.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const createError = require('http-errors');
 
 router.get('/', async function (req, res, next) {
-  res.status(200).send('Ok');
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
