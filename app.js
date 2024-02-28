7f473f0b90108d4fb4edbb33b4e2ad7605639c79
woongminKi <dndals1991@gmail.com>
2024-02-28 22:12:48 +0900

    에러 해결 위한 테스트4

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   res.status(err.status || 500);
-  res.render('error');
+  res.json({ message: err.message, status: err.status });
 });
 
 module.exports = app;
